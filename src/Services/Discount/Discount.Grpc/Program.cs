7ef2c695b549d190c0108fd5d6218b50a3bd11d0
hoang-rasta <hoanganhpham.rasta@gmail.com>
2026-04-20 23:20:29 +0700

    Implement Discount microservice with gRPC support and PostgreSQL integration

---
+++ src/Services/Discount/Discount.Grpc/Program.cs
@@ -0,0 +1,32 @@
+using Discount.Grpc.Repositories;
+using Discount.Grpc.Repositories.Interfaces;
+using Discount.Grpc.Services;
+
+namespace Discount.Grpc
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddScoped<IDiscountRepository, DiscountRepository>();
+            builder.Services.AddAutoMapper(typeof(Program));
+            builder.Services.AddGrpc();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            //app.MapGrpcService<GreeterService>();
+            app.MapGrpcService<DiscountService>();
+            app.MapGet(
+                "/",
+                () =>
+                    "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909"
+            );
+
+            app.Run();
+        }
+    }
+}
